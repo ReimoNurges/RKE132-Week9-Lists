aa18f8d40d1f25c05a27d0270e2e10405b957d27
reimo <reimo.nurges@tktk.ee>
2024-11-20 09:26:03 +0200

    First comment

---
+++ Week9-Lists/Program.cs
@@ -0,0 +1,59 @@
+﻿string folderPath = @"C:\C#\dasdsadsad";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+static List<string> GetItemsFromUser()
+{
+
+
+    List<string> userList = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine("Add item (add)/ Exit (exit): ");
+        string usercoise = Console.ReadLine();
+
+        if (usercoise == "add")
+        {
+            Console.WriteLine("Enter an item: ");
+            string useritem = Console.ReadLine();
+            userList.Add(useritem);
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+
+        }
+    }
+    return userList;
+}
+
+static void ShowItemsFromList(List<string> someList)
+{
+
+    int listlenght = someList.Count;
+    Console.WriteLine($"You have added {listlenght} items to your shopping list");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+        Console.WriteLine($"{i}. {item}");
+        i++;
+    }
+
+
+}
